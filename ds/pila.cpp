e755dd168073f1d5641555d3bf562bf1d2ca05a6
Diego Gil <diego_gilhalo98@hotmail.com>
2023-01-08 03:23:24 -0600

    pila modificada

--- ds/pila.cpp
+++ ds/pila.cpp
@@ -1,141 +1,104 @@
-#include "DS.h"
+#include "comun.h"
+#include "estructuras.h"
+
+// Constructor de clase.
+template <class T>
+ESTRUCTURA::PILA<T>::PILA(int limiteDatos) {
+    // Cantida de datos maixmo de la estructura.
+    this->limiteDatos = limiteDatos;
+
+    // Cantidad de datos actual de la estructura.
+    this->cantidadDatos = 0;
+
+    // Puntero al elemento superior de la estructura.
+    this->punteroSuperior = NULL;
+};
+
+// Destructor de clase.
+template <class T>
+ESTRUCTURA::PILA<T>::~PILA(void) {};
+
+// Agrega un elemento a la estructura.
+template <class T>
+void ESTRUCTURA::PILA<T>::push(T dato) {
+    // Si la pila no esta llena, agregamos el nodo.
+    if(!this->estaLlena()) {
+        // Se crea un nuevo nodo.
+        nodoSimple<T>* nodoNuevo = new nodoSimple<T>();
+
+        // Se asigna el index y el dato al nodo.
+        nodoNuevo->dato = dato;
+
+        // Si la pila no esta vacia, se conecta el nodo a
+        // la estructura.
+        if(!this->estaVacia()) {
+            nodoNuevo->siguiente = this->punteroSuperior;
+        }
+ 
+        // Actualizamos el puntero superior.
+        this->punteroSuperior = nodoNuevo;
+
+        // Aumentamos el conteo en 1.
+        this->cantidadDatos++;
+    }
+};
+
+// Retira un elemento de la estructura.
+template <class T>
+T ESTRUCTURA::PILA<T>::pop(void) {
+    // Mientras que la pila no este vacia.
+    if(!this->estaVacia()) {
+        // Almacena una varible con el dato del nodo.
+        T dato;
+
+        // Si hay mas de un elemento, se instancia un puntero auxiliar.
+        nodoSimple<T>* punteroNodoRemovido = this->punteroSuperior;
+
+        // Movemos el nodo superior una posicion.
+        this->punteroSuperior = this->punteroSuperior->siguiente;
+
+        // Recuperamos el dato del nodo a remover.
+        dato = punteroNodoRemovido->dato;
+
+        // se elimina el nodo en el puntero auxiliar.
+        delete punteroNodoRemovido;
+
+        // Disminuimos en uno el conteo de elementos.
+        this->cantidadDatos--;
+
+        // Retornamos los datos recuperados.
+        return dato;
+
+    } else {
+        // Si la pila esta vacia, retorna NULL;
+        return (T)NULL;
+    }
+};
+
+// Funcion para verificar el funcionamiento de la estructura.
+template <class T>
+void ESTRUCTURA::PILA<T>::debug(void) {
+    // Si la estructura no esta vacia, se imprime
+    // su contendio.
+    if(!this->estaVacia()) {
+        nodoSimple<T>* punteroAux = this->punteroSuperior;
+
+        while(punteroAux->siguiente != NULL) {
+            std::cout<<punteroAux->dato<<", ";
+            punteroAux = punteroAux->siguiente;
+        }
+        std::cout<<punteroAux->dato<<std::endl;
+    }
+    // Sino se imprime que esta vacia.
+    else {
+        std::cout<<"Vacia!"<<std::endl;
+    }
+};
 
 template <class T>
-ESTRUCTURAS::PILA<T>::PILA(int capacidad) {
-	// Por default, la capacidad es 0, esto indica
-	// que la pila no tiene un maximo de elementos.
-	this->capacidadDatos = capacidad;
-
-	// Cantidad de datos actual en la pila.
-	this->cantidadDatos = 0;
-
-	// Puntero al elemento superior de la pila.
-	this->punteroSuperior = NULL;
-}
-
-template <class T>
-ESTRUCTURAS::PILA<T>::~PILA(void) {}
-
-template <class T>
-bool ESTRUCTURAS::PILA<T>::push(T dato) {
-	// Agregamos un nodo a la pila.
-
-	// Verificamos que la pila no este llena.
-	if(!this->estaLlena()) {
-		// Creamos un nuevo nodo.
-		NodoSimple<T>* nuevoNodo = new NodoSimple<T>();
-
-		// Agregamos el dato al nodo.
-		nuevoNodo->dato = dato;
-
-		// Verificamos si la pila esta vacia.
-		if(this->estaVacia()) {
-			// Asignamos el puntero superior al nuevo nodo.
-			this->punteroSuperior = nuevoNodo;
-		} else {
-			// Colocamos el nuevo nodo
-			// encima del puntero superior
-			nuevoNodo->siguiente = this->punteroSuperior;
-
-			// Asignamos el nuevo nodo
-			// como el puntero superor.
-			this->punteroSuperior = nuevoNodo;
-		}
-
-		// Aumentamos el conteo de datos.
-		this->cantidadDatos++;
-
-		return true;
-	}
-	return false;
-}
-
-template <class T>
-T ESTRUCTURAS::PILA<T>::pop() {
-	// Remueve un dato de la pila.
-	if(!this->estaVacia()) {
-		// Dato que se recupera al eliminar el nodo.
-		T dato;
-
-		// Nodo a eliminar.
-		NodoSimple<T>* nodoRemovido = this->punteroSuperior;
-
-		// El puntero superior se mueve al siguiente nodo.
-		this->punteroSuperior = this->punteroSuperior->siguiente;
-
-		// Dato recuperado.
-		dato = nodoRemovido->dato;
-
-		// Liberamos la memoria usada.
-		delete nodoRemovido;
-
-		// Disminuye la cantida de datos en la pila.
-		this->cantidadDatos--;
-
-		// Retornamos el dato.
-		return dato;
-	}
-
-	return (T)NULL;
-}
-
-template <class T>
-bool ESTRUCTURAS::PILA<T>::dump() {
-	// Vacia la pila.
-	while(this->cantidadDatos >0) {
-		this->pop();
-	}
-
-	return true;
-}
-
-template <class T>
-int ESTRUCTURAS::PILA<T>::getCantidadDatos() {
-	// Retorna la cantida de datos actual en
-	// la pila.
-	return this->cantidadDatos;
-}
-
-template <class T>
-int ESTRUCTURAS::PILA<T>::getCapacidad() {
-	// Retorna la capacidad de la pila
-	// si retorna 0 no hay un limite.
-	return this->capacidadDatos;
-}
-
-template <class T>
-bool ESTRUCTURAS::PILA<T>::estaVacia() {
-	// Retorna si la pila esta vacia.
-	if(this->cantidadDatos == 0) {
-		return true;
-	}
-
-	return false;
-}
-
-template <class T>
-bool ESTRUCTURAS::PILA<T>::estaLlena() {
-	// Retorna si la pila esta llena.
-	if(this->capacidadDatos > 0) {
-		if(this->cantidadDatos == this->capacidadDatos) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-template <class T>
-void ESTRUCTURAS::PILA<T>::debug() {
-	if(!this->estaVacia()) {
-		NodoSimple<T>* punteroAux = this->punteroSuperior;
-
-		while(punteroAux->siguiente != NULL) {
-			std::cout<<punteroAux->dato<<", ";
-			punteroAux = punteroAux->siguiente;
-		}
-		std::cout<<punteroAux->dato<<std::endl;
-	} else {
-		std::cout<<"Vacia!"<<std::endl;
-	}
-}
+void ESTRUCTURA::PILA<T>::dump(void) {
+    // Vacia todos los nodos de la estructura.
+    while(!this->estaVacia()) {
+        this->pop();
+    }
+};
