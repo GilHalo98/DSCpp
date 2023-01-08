cd2a68ebee978f23c68783c7e268179e398e52a6
Diego Gil <diego_gilhalo98@hotmail.com>
2023-01-08 03:24:16 -0600

    Cabezera comun agregada

---
+++ ds/comun.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// Manejor de variables booleanas.
+#include <stdbool.h>
+
+
+// Incluye los punteros para las estructuras de datos.
+#include "nodos.cpp"
+#include "estructuras.h"
+#include "estructuraBase.cpp"
