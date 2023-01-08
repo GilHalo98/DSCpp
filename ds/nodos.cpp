8859a767c4441376a83a5c57ed21e239cf25d6ff
Diego Gil <diego_gilhalo98@hotmail.com>
2023-01-08 03:21:45 -0600

    Nombres de nodos cambiados

--- ds/nodos.cpp
+++ ds/nodos.cpp
@@ -1,12 +1,40 @@
 /*
- * Nodos de las estructuras de datos.
- * */
+ * Nodos que usan las estructuras de datos
+ * estos nodos estan contruidos por templates.
+*/
 
+
+// Nodo simple, contiene un puntero.
 template <class T>
-struct NodoSimple {
-	// Dato del nodo.
-	T dato;
+struct nodoSimple {
+    // Dato que almacena el nodo.
+    T dato;
 
-	// Puntero siguiente del nodo.
-	NodoSimple* siguiente = NULL;
+    // Punteros usados para estructurar nodos.
+    nodoSimple* siguiente = NULL;
+};
+
+
+// Nodo para los arboles binarios.
+template <class T>
+struct nodoBinario {
+    // Dato del nodo.
+    T dato;
+
+    // Punteros del nodo.
+    nodoBinario* nodoPadre = NULL;
+    nodoBinario* hijoDerecha = NULL;
+    nodoBinario* hijoIzquierda = NULL;
+};
+
+
+// Nodo doble, contiene dos punteros.
+template <class T>
+struct nodoDoble {
+    // Dato del nodo.
+    T dato;
+
+    // Punteros del nodo.
+    nodoDoble* siguiente = NULL;
+    nodoDoble* anterior = NULL;
 };
