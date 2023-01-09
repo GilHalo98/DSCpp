1eace54d59545365ccf5c509d5d606cf2b2c1eaa
Diego Gil <diego_gilhalo98@hotmail.com>
2023-01-08 22:16:33 -0600

    Tutorial de uso de arbol, lista doble y lista agregado

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "ds/pila.cpp"
 #include "ds/cola.cpp"
+#include "ds/lista.cpp"
+#include "ds/listaDoble.cpp"
+#include "ds/arbolBinario.cpp"
 
 
 int main() {
@@ -36,6 +39,53 @@ int main() {
     while(!cola.estaVacia()) {
         std::cout<<cola.pop()<<", ";
     }
+    std::cout<<std::endl;
+
+    std::cout<<"Lista"<<std::endl;
+    ESTRUCTURA::LISTA<int> lista = ESTRUCTURA::LISTA<int>(10);
+
+    i = 0;
+    while(i < lista.getLimiteDatos()) {
+        lista.push(i);
+        i++;
+    }
+
+    lista.debug();
+
+    while(!lista.estaVacia()) {
+        std::cout<<lista.pop()<<", ";
+    }
+    std::cout<<std::endl;
+
+    std::cout<<"Lista Doble"<<std::endl;
+    ESTRUCTURA::LISTA_DOBLE<int> listaDoble = ESTRUCTURA::LISTA_DOBLE<int>(10);
+
+    i = 0;
+    while(i < listaDoble.getLimiteDatos()) {
+        listaDoble.push(i);
+        i++;
+    }
+
+    listaDoble.debug();
+
+    while(!listaDoble.estaVacia()) {
+        std::cout<<listaDoble.pop()<<", ";
+    }
+    std::cout<<std::endl;
+
+    std::cout<<"Arbol Binario"<<std::endl;
+    ESTRUCTURA::ARBOL_BINARIO<int> arbolBinario = ESTRUCTURA::ARBOL_BINARIO<int>(10);
+
+    i = 0;
+    while(i < arbolBinario.getLimiteDatos()) {
+        arbolBinario.push(i);
+        i++;
+    }
+
+    while(!arbolBinario.estaVacia()) {
+        std::cout<<arbolBinario.pop()<<", ";
+    }
+    std::cout<<std::endl;
 
     return 1;
 };
