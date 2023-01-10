116ac19371f8ff4746b339b3045cd307492398f8
Diego Gil <diego_gilhalo98@hotmail.com>
2023-01-09 18:02:58 -0600

    Reocrridos del arbol binario agregado

---
+++ ds/recorridosArbolBinario.cpp
@@ -0,0 +1,86 @@
+#include "comun.h"
+
+template <class T>
+ESTRUCTURA::COLA<T> RECORRIDOS::recorridoArbolBinario(
+    ESTRUCTURA::ARBOL_BINARIO<T> arbolBinario,
+    RECORRIDOS::TIPOS tipoRecorrido
+) {
+    // Realiza los recorridos a los arboles binarios, los tipos
+    // aceptados son; inorden, postorden, preorden.
+    
+    // Declaramos la cola del recorrido.
+    ESTRUCTURA::COLA<T> colaRecorrido = ESTRUCTURA::COLA<T>(
+        arbolBinario.getCantidadDatos()
+    );
+
+    // Switch/Case de los distintos tipos de recorridos validos.
+    switch(tipoRecorrido) {
+        case RECORRIDOS::TIPOS::PREORDEN:
+            RECORRIDOS::preorden(
+                arbolBinario.getPunteroRaiz(),
+                &colaRecorrido
+            );
+            break;
+
+        case RECORRIDOS::TIPOS::POSTORDEN:
+            RECORRIDOS::postorden(
+                arbolBinario.getPunteroRaiz(),
+                &colaRecorrido
+            );
+            break;
+
+        case RECORRIDOS::TIPOS::INORDEN:
+            RECORRIDOS::inorden(
+                arbolBinario.getPunteroRaiz(),
+                &colaRecorrido
+            );
+            break;
+
+        default:
+            throw std::invalid_argument(
+                "ERROR DE OPCION: Tipo de recorrido invalido!"
+            );
+            break;
+    } 
+
+    return colaRecorrido;
+};
+
+template <class T>
+void RECORRIDOS::preorden(
+    nodoBinario<T>* punteroAux,
+    ESTRUCTURA::COLA<T>* colaRecorrido
+) {
+    // Recorrido preorden del arbol binario.
+    if(punteroAux != NULL) {
+        colaRecorrido->push(punteroAux->dato);
+        RECORRIDOS::preorden(punteroAux->hijoIzquierda, colaRecorrido);
+        RECORRIDOS::preorden(punteroAux->hijoDerecha, colaRecorrido);
+    }
+};
+
+template <class T>
+void RECORRIDOS::postorden(
+    nodoBinario<T>* punteroAux,
+    ESTRUCTURA::COLA<T>* colaRecorrido
+) {
+    // Recorrido postorden del arbol binario.
+    if(punteroAux != NULL) {
+        RECORRIDOS::postorden(punteroAux->hijoIzquierda, colaRecorrido);
+        RECORRIDOS::postorden(punteroAux->hijoDerecha, colaRecorrido);
+        colaRecorrido->push(punteroAux->dato);
+    }
+};
+
+template <class T>
+void RECORRIDOS::inorden(
+    nodoBinario<T>* punteroAux,
+    ESTRUCTURA::COLA<T>* colaRecorrido
+) {
+    // Recorrido inorden del arbol binario.
+    if(punteroAux != NULL) {
+        RECORRIDOS::inorden(punteroAux->hijoIzquierda, colaRecorrido);
+        colaRecorrido->push(punteroAux->dato);
+        RECORRIDOS::inorden(punteroAux->hijoDerecha, colaRecorrido);
+    }
+};
